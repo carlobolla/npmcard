d349327d17c751ef389733900f37950a5bd4b838
Carlo Bollini (@carlobolla) <carlobolla@live.com>
2021-04-16 12:48:35 +0200

    Updated and edited style :)

--- bin/card.js
+++ bin/card.js
@@ -9,20 +9,21 @@ const boxen = require('boxen')
 let options = {
   padding: 1,
   margin: 1,
-  borderStyle: 'round'
+  borderStyle: 'round',
+  borderColor: 'cyan'
 }
 // Text + chalk definitions
 let data = {
-  'name': chalk.white('Carlo Bollini /'),
-  'handle': chalk.cyan('carlobolla'),
+  'name': chalk.white.bold('Carlo Bollini /'),
+  'handle': chalk.green.bold('carlobolla'),
   'work': chalk.white('Student Full-stack Developer'),
-  'skills': chalk.white('Python, C++, Java, PHP'),
-  'learning': chalk.white('NodeJS, Go'),
-  'twitter': chalk.cyan('https://twitter.com/carlobolla'),
-  'github': chalk.cyan('https://github.com/carlobolla'),
-  'linkedin': chalk.cyan('https://www.linkedin.com/in/carlo-bollini'),
-  'web': chalk.cyan('https://carlobolla.xyz'),
-  'npx': chalk.white('npx carlobolla'),
+  'skills': chalk.white('Python, MERN stack, C#, Java'),
+  'learning': chalk.white('Go'),
+  'twitter': chalk.green.underline('https://twitter.com/carlobolla'),
+  'github': chalk.green.underline('https://github.com/carlobolla'),
+  'linkedin': chalk.green.underline('https://www.linkedin.com/in/carlo-bollini'),
+  'web': chalk.green.underline('https://carlobolla.xyz'),
+  'npx': chalk.green('npx carlobolla'),
   'labelWork': chalk.white.bold('      Work:'),
   'labelSkills': chalk.white.bold('    Skills:'),
   'labelLearning': chalk.white.bold('  Learning:'),
